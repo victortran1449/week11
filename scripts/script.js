bf24b9ae830770967fe654e035662c7d258189c5
Victor Tran <victortran1449@gmail.com>
2022-11-16 12:42:22 -0800

    function declaration practice

---
+++ scripts/script.js
@@ -0,0 +1,23 @@
+// let userName = prompt("What is your name?");
+
+// function greet(user) {
+//   console.log(`Hello ${user}!`);
+// }
+
+// greet(userName);
+ 
+
+// const greet = function (user) {
+//     console.log(`Hello ${user}!`);
+//   }
+
+// greet("Victor")
+
+const greet = (user) => {
+    console.log(`Hello ${user}!`);
+  }
+
+greet("Victor")
+
+
+  
