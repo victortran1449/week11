86282c248daa35728fa3bb5603c2da6f44ae1b82
Victor Tran <victortran1449@gmail.com>
2022-11-16 13:08:09 -0800

    function practice with two parameters

--- scripts/script.js
+++ scripts/script.js
@@ -13,10 +13,17 @@
 
 // greet("Victor")
 
-const greet = (user) => {
-    console.log(`Hello ${user}!`);
+// const greet = (user) => {
+//     console.log(`Hello ${user}!`);
+//   }
+
+// greet("Victor")
+
+const greet = (user, time = "day") => {
+    console.log(`Good ${time} ${user}!`);
   }
 
+greet("Victor", "afternoon")
 greet("Victor")
 
 
